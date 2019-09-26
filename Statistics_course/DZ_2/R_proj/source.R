c8d23411e9c1352619290686ec4dd6b790e13229
hazirliver <36604387+hazirliver@users.noreply.github.com>
2019-09-26 23:38:40 +0300

    HW1_2_upd

--- Statistics_course/DZ_2/R_proj/source.R
+++ Statistics_course/DZ_2/R_proj/source.R
@@ -1,24 +1,78 @@
+x <-xtable(B,align=rep("",ncol(B)+1), digits = 3)
+print(x, floating=FALSE, tabular.environment="bmatrix", 
+      hline.after=NULL, include.rownames=FALSE, include.colnames=FALSE)
+
+library(xtable)
 k <- 8
 p1 <- 0.7
 n <- 140
-p <- rep(NA, k+1)
-q <- rep(NA, k+1)
 
-p <- dbinom(c(0:8), 8, 0.7)
+distr_series_table <- as.data.frame(rbind(c(0:k),
+                            dbinom(c(0:k), k, p1)),
+                            row.names = c("Random Value", "Probability")) 
+colnames(distr_series_table) <- c(0:k)
+sum(distr_series_table[2,])
+distr_series_table
+
+distr_series_table <- rbind(distr_series_table,
+                            cumsum(dbinom(c(0:k), k, p1)))
+row.names(distr_series_table)[3] <- "Delta"
+distr_series_table
 
 
-q <- qbinom(c(0:8), 8, 0.7)
+x <-xtable(distr_series_table,align=rep("",ncol(distr_series_table)+1), digits = 3)
+print(x, floating=FALSE, tabular.environment="bmatrix", 
+      hline.after=NULL, include.rownames=T, include.colnames=F)
+
+sum(distr_series_table[2,])
+
+distr_series_table <- rbind(distr_series_table)
+
+set.seed(1337)
+rand_unif <- runif(n, 0, 1)
+head(rand_unif)
+
+emp_sample <- ifelse(rand_unif < distr_series_table[3,1], 0, 
+      ifelse(rand_unif < distr_series_table[3,2] & rand_unif >= distr_series_table[3,2-1], 1, 
+      ifelse(rand_unif < distr_series_table[3,3] & rand_unif >= distr_series_table[3,3-1], 2, 
+      ifelse(rand_unif < distr_series_table[3,4] & rand_unif >= distr_series_table[3,4-1], 3, 
+      ifelse(rand_unif < distr_series_table[3,5] & rand_unif >= distr_series_table[3,5-1], 4,
+      ifelse(rand_unif < distr_series_table[3,6] & rand_unif >= distr_series_table[3,6-1], 5,
+      ifelse(rand_unif < distr_series_table[3,7] & rand_unif >= distr_series_table[3,7-1], 6,
+      ifelse(rand_unif < distr_series_table[3,8] & rand_unif >= distr_series_table[3,8-1], 7,
+      ifelse(rand_unif < distr_series_table[3,9] & rand_unif >= distr_series_table[3,9-1], 8,NA)))))))))
+
+emp_sample
+
+##
+sum(emp_sample == 2)
+hist_tmp <- hist(emp_sample, breaks = c(0:k))
+hist_tmp$counts
+hist_tmp$breaks
+
+stat_series <- as.data.frame(rbind(c(0:k), c(0, hist_tmp$counts), (c(0, hist_tmp$counts) / n) ), row.names = c("Simulated values", "Frequencies", "Relative frequencies"))
+colnames(stat_series) <- c(0:8)
+
+
+######################################
+
+p <- dbinom(c(0:k), k, p1)
+cumsum(p)
+cumsum(as.list(distr_series_table[2,]))
+
+
+q <- pbinom(c(0:8), 8, 0.7)
 set.seed(161)
-y <- runif(n, 0, 1)
+rand_unif <- runif(n, 0, 1)
 p_p <- rep(0,k+2)
 
 
-p_p[1] <- sum(y < q[1])
+p_p[1] <- sum(rand_unif < q[1])
 
 
 for (i in 2:(k+2))
 {
-  p_p[i] <- sum(ifelse(y < q[i] & y >= q[i-1], 1, 0))
+  p_p[i] <- sum(ifelse(rand_unif < q[i] & rand_unif >= q[i-1], 1, 0))
 }
 p_p
 #sum(p_p)
@@ -26,15 +80,15 @@ p_p
 
 q_q <- rep(NA,k+1)
 
-q_q <- ifelse(y < q[2], 0, 
-       ifelse(y < q[3] & y >= q[3-1], 1, 
-       ifelse(y < q[4] & y >= q[4-1], 2, 
-       ifelse(y < q[5] & y >= q[5-1], 3, 
-       ifelse(y < q[6] & y >= q[6-1], 4,
-       ifelse(y < q[7] & y >= q[7-1], 5,
-       ifelse(y < q[8] & y >= q[8-1], 6,
-       ifelse(y < q[9] & y >= q[9-1], 7,
-       ifelse(y < q[10] & y >= q[10-1], 8,NA)))))))))
+q_q <- ifelse(rand_unif < q[2], 0, 
+       ifelse(rand_unif < q[3] & rand_unif >= q[3-1], 1, 
+       ifelse(rand_unif < q[4] & rand_unif >= q[4-1], 2, 
+       ifelse(rand_unif < q[5] & rand_unif >= q[5-1], 3, 
+       ifelse(rand_unif < q[6] & rand_unif >= q[6-1], 4,
+       ifelse(rand_unif < q[7] & rand_unif >= q[7-1], 5,
+       ifelse(rand_unif < q[8] & rand_unif >= q[8-1], 6,
+       ifelse(rand_unif < q[9] & rand_unif >= q[9-1], 7,
+       ifelse(rand_unif < q[10] & rand_unif >= q[10-1], 8,NA)))))))))
 length(q_q)
 mean(q_q)
 
@@ -82,9 +136,9 @@ pbinom(100, 140, 0.7)
 
 
 length(q_q / k)
-length(y)
+length(rand_unif)
 
-y*k
+rand_unif*k
 q_q
 
 ks_test <- ks.test(unique(sort(q_q / k)), pbinom(0.1, 140, 0.7))
