9e9ab8a687379f2ca92e09ec7b989bc1c0071e98
hazirliver <36604387+hazirliver@users.noreply.github.com>
2019-10-13 12:04:21 +0300

    upd

--- Statistics_course/DZ_4/R_proj/source.R
+++ Statistics_course/DZ_4/R_proj/source.R
@@ -1,7 +1,7 @@
 k <- 8
 p1 <- 0.7
 n <- 140
-
+set.seed(1337)
 distr_series_table <- as.data.frame(rbind(c(0:k),
                             dbinom(c(0:k), k, p1)),
                             row.names = c("Random Value", "Probability")) 
@@ -20,7 +20,7 @@ sum(distr_series_table[2,])
 
 distr_series_table <- rbind(distr_series_table)
 
-set.seed(1337)
+
 rand_unif <- runif(n, 0, 1)
 head(rand_unif)
 
@@ -40,25 +40,76 @@ emp_sample
 
 
 ################################################################################
-cp_ci_LB.01 <- qbeta(0.1/2, sum(emp_sample), n*k - sum(emp_sample) + 1)
-cp_ci_LB.005 <- qbeta(0.05/2, sum(emp_sample), n*k - sum(emp_sample) + 1)
-cp_ci_LB.002 <- qbeta(0.02/2, sum(emp_sample), n*k - sum(emp_sample) + 1)
+cp_ci_LB.01 <- qbeta(0.1/2, sum(emp_sample), 
+                     n*k - sum(emp_sample) + 1)
+cp_ci_LB.005 <- qbeta(0.05/2, sum(emp_sample), 
+                      n*k - sum(emp_sample) + 1)
+cp_ci_LB.002 <- qbeta(0.02/2, sum(emp_sample), 
+                      n*k - sum(emp_sample) + 1)
 
-cp_ci_UB.01 <- qbeta(1 - 0.1/2, sum(emp_sample) + 1, n*k - sum(emp_sample))
-cp_ci_UB.005 <- qbeta(1 - 0.05/2, sum(emp_sample) + 1, n*k - sum(emp_sample))
-cp_ci_UB.002 <- qbeta(1 - 0.02/2, sum(emp_sample) + 1, n*k - sum(emp_sample))
+cp_ci_UB.01 <- qbeta(1 - 0.1/2, sum(emp_sample) + 1, 
+                     n*k - sum(emp_sample))
+cp_ci_UB.005 <- qbeta(1 - 0.05/2, sum(emp_sample) + 1, 
+                      n*k - sum(emp_sample))
+cp_ci_UB.002 <- qbeta(1 - 0.02/2, sum(emp_sample) + 1, 
+                      n*k - sum(emp_sample))
+
+cp_cis <- as.data.frame( matrix(c(cp_ci_LB.01, cp_ci_UB.01,
+                                  cp_ci_LB.005, cp_ci_UB.005,
+                                  cp_ci_LB.002, cp_ci_UB.002), 
+                                byrow = T, ncol = 2))
+colnames(cp_cis) <- c("Lower", "Upper")
+rownames(cp_cis) <- c("0.1", "0.05", "0.02")
+cp_cis
+##################################################
+
+##################################################
+stat1 <- sum(emp_sample)
+stat1
+uup <- function(theta) qbinom(0.02/2, n*k, theta)
+lou <- function(theta) qbinom(1 - 0.02/2, n*k, theta)
+png(filename = "../img/quantiles.png", 
+    width = 1600, height = 1200,
+    pointsize = 24, res = 96)
+plot(uup, col = "red", xlab = "",ylab = "", lwd = 3)
+plot(lou, col = "blue", lwd = 3, add = T)
+abline(h = stat1, col = "green", lwd = 2, lty = 2)
+legend("bottomright", 
+       c("Quantile 0.02", "Quantile 1-0.02", "806"), 
+       lty=c(1,1,2), 
+       fill=c("red", "blue", "green"))
+dev.off()
+####################################################
+
+####################################################
+
+foo <- function(p) (sum(emp_sample) - n*k*p)/(sqrt(n*k*p*(1-p)))
+png(filename = "../img/foo.png", 
+    width = 1600, height = 1200,
+    pointsize = 24, res = 96)
+plot(foo, col = "red", lwd = 2, xlab = "p", ylab = "foo(p)")
+dev.off()
 
 
+
+
+#####################################################
 library(rootSolve)
 
-clt_ci_LB.01_foo <- function(p) (sum(emp_sample) - n*k*p)/(sqrt(n*k*p*(1-p))) - qnorm(1-0.1/2)
-clt_ci_UB.01_foo <- function(p) (sum(emp_sample) - n*k*p)/(sqrt(n*k*p*(1-p))) + qnorm(1-0.1/2)
+clt_ci_LB.01_foo <- function(p) (sum(emp_sample) - n*k*p)/
+  (sqrt(n*k*p*(1-p))) - qnorm(1-0.1/2)
+clt_ci_UB.01_foo <- function(p) (sum(emp_sample) - n*k*p)/
+  (sqrt(n*k*p*(1-p))) + qnorm(1-0.1/2)
 
-clt_ci_LB.005_foo <- function(p) (sum(emp_sample) - n*k*p)/(sqrt(n*k*p*(1-p))) - qnorm(1-0.05/2)
-clt_ci_UB.005_foo <- function(p) (sum(emp_sample) - n*k*p)/(sqrt(n*k*p*(1-p))) + qnorm(1-0.05/2)
+clt_ci_LB.005_foo <- function(p) (sum(emp_sample) - n*k*p)/
+  (sqrt(n*k*p*(1-p))) - qnorm(1-0.05/2)
+clt_ci_UB.005_foo <- function(p) (sum(emp_sample) - n*k*p)/
+  (sqrt(n*k*p*(1-p))) + qnorm(1-0.05/2)
 
-clt_ci_LB.002_foo <- function(p) (sum(emp_sample) - n*k*p)/(sqrt(n*k*p*(1-p))) - qnorm(1-0.02/2)
-clt_ci_UB.002_foo <- function(p) (sum(emp_sample) - n*k*p)/(sqrt(n*k*p*(1-p))) + qnorm(1-0.02/2)
+clt_ci_LB.002_foo <- function(p) (sum(emp_sample) - n*k*p)/
+  (sqrt(n*k*p*(1-p))) - qnorm(1-0.02/2)
+clt_ci_UB.002_foo <- function(p) (sum(emp_sample) - n*k*p)/
+  (sqrt(n*k*p*(1-p))) + qnorm(1-0.02/2)
 
 clt_ci_LB.01 <- uniroot(clt_ci_LB.01_foo, c(0.1, 0.9))$root
 clt_ci_UB.01 <- uniroot(clt_ci_UB.01_foo, c(0.1, 0.9))$root
@@ -69,23 +120,34 @@ clt_ci_UB.005 <- uniroot(clt_ci_UB.005_foo, c(0.1, 0.9))$root
 clt_ci_LB.002 <- uniroot(clt_ci_LB.002_foo, c(0.1, 0.9))$root
 clt_ci_UB.002 <- uniroot(clt_ci_UB.002_foo, c(0.1, 0.9))$root
 
+clt_cis <- as.data.frame( matrix(c(clt_ci_LB.01, clt_ci_UB.01,
+                                  clt_ci_LB.005, clt_ci_UB.005,
+                                  clt_ci_LB.002, clt_ci_UB.002), 
+                                byrow = T, ncol = 2))
+colnames(clt_cis) <- c("Lower", "Upper")
+rownames(clt_cis) <- c("0.1", "0.05", "0.02")
+clt_cis
 
 compare_CI <- as.data.frame(matrix(c(clt_ci_LB.01, cp_ci_LB.01, 
                                      clt_ci_UB.01, cp_ci_UB.01,
                                      clt_ci_LB.005, cp_ci_LB.005,
                                      clt_ci_UB.005, cp_ci_UB.005,
                                      clt_ci_LB.002, cp_ci_LB.002,
-                                     clt_ci_UB.002, cp_ci_UB.002), ncol = 2, byrow = T))
+                                     clt_ci_UB.002, cp_ci_UB.002), 
+                                   ncol = 2, byrow = T))
 
 
 theor_distr <- rbinom(n, k, p1)
-distr_UB <- rbinom(n, k, cp_ci_UB.01)
-distr_LB <- rbinom(n, k, cp_ci_LB.01)
+distr_UB <- rbinom(n, k, cp_ci_UB.002)
+distr_LB <- rbinom(n, k, cp_ci_LB.002)
 
+png(filename = "../img/cdf_with_cis_1.png", 
+    width = 1600, height = 1200,
+    pointsize = 24, res = 96)
 plot(ecdf(theor_distr), 
      col = "red", lwd = 3, verticals = T, axes = F,
      xlim = c(0,k+1), ylim = c(0,1.2),
-     xlab = "Value", ylab = "CDF", main = "Theoretical and Emperical CDF")
+     xlab = "Value", ylab = "CDF", main = "CDF with CIs")
 plot(ecdf(distr_LB), 
      col = "black", lwd = 2, verticals = T, add = T)
 plot(ecdf(distr_UB), 
@@ -96,6 +158,6 @@ grid(nx = k+1, ny = 1.2 / 0.2)
 legend("bottomright", c("Theoretical", "Lower", "Upper"), 
        lty=c(1,1,1), 
        fill=c("red", "black", "blue"))
-
+dev.off()
 
 
