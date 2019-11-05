dfcfe11bd802fa7df31ca2adce417ef8c05a9630
hazirliver_notebook <cs-mitino@mail.ru>
2019-11-05 18:28:13 +0300

    upd

---
+++ Numerical_analysis/Lab_4/R_proj/.Rproj.user/B725F991/sources/per/t/D45C3521-contents
@@ -0,0 +1,117 @@
+A <- matrix(c(12,1,2,3,
+              1,12,3,-2,
+              2,3,12,1,
+              3,-2,1,12), byrow = T, ncol = 4)
+phi <- 1/2 * atan(1/((A[1,1] -A[4,4])/(2 * A[1,4])))
+
+Q <- matrix(c(cos(phi),0,0,-sin(phi),
+              0,1,0,0,
+              0,0,1,0,
+              sin(phi),0,0,cos(phi)), byrow = T, ncol = 4)
+Q_t <- t(Q)
+
+A1 <- Q_t %*% A %*% Q
+
+
+
+############
+phi1 <- 1/2 * atan(1/((A1[2,2] -A1[3,3])/(2 * A1[2,3])))
+
+Q1 <- matrix(c(1,0,0,0,
+               0,cos(phi1),-sin(phi1),0,
+               0,sin(phi1),cos(phi1),0,
+               0,0,0,1), byrow = T, ncol = 4)
+Q1_t <- t(Q1)
+
+A2 <- Q1_t %*% A1 %*% Q1
+
+
+
+
+############
+phi2 <- 1/2 * atan(1/((A2[1,1] -A2[3,3])/(2 * A2[1,3])))
+
+Q2 <- matrix(c(cos(phi2),0,-sin(phi2),0,
+               0,1,0,0,
+               sin(phi2),0,cos(phi2),0,
+               0,0,0,1), byrow = T, ncol = 4)
+
+Q2_t <- t(Q2)
+
+A3 <- Q2_t %*% A2 %*% Q2
+
+
+
+
+
+
+
+
+
+##############
+phi3 <- 1/2 * atan(1/((A3[2,2] -A3[4,4])/(2 * A3[2,4])))
+
+Q3 <- matrix(c(1,0,0,0,
+               0,cos(phi3),0,-sin(phi3),
+               0,0,1,0,
+               0,sin(phi3),0,cos(phi3)), byrow = T, ncol = 4)
+
+Q3_t <- t(Q3)
+
+A4 <- Q3_t %*% A3 %*% Q3
+
+
+##############
+phi4 <- 1/2 * atan(1/((A4[3,3] -A4[4,4])/(2 * A4[3,4])))
+
+Q4 <- matrix(c(1,0,0,0,
+               0,1,0,0,
+               0,0,cos(phi4),-sin(phi4),
+               0,0,sin(phi4),cos(phi4)), byrow = T, ncol = 4)
+
+Q4_t <- t(Q4)
+
+A5 <- Q4_t %*% A4 %*% Q4
+
+
+
+##############
+phi5 <- 1/2 * atan(1/((A5[1,1] -A5[2,2])/(2 * A5[1,2])))
+
+Q5 <- matrix(c(cos(phi5),-sin(phi5),0,0,
+               sin(phi5),cos(phi5),0,0,
+               0,0,1,0,
+               0,0,0,1), byrow = T, ncol = 4)
+
+Q5_t <- t(Q5)
+
+A6 <- Q5_t %*% A5 %*% Q5
+
+
+
+##############
+phi6 <- 1/2 * atan(1/((A6[1,1] -A6[4,4])/(2 * A6[1,4])))
+
+Q6 <- matrix(c(cos(phi6),0,0,-sin(phi6),
+              0,1,0,0,
+              0,0,1,0,
+              sin(phi6),0,0,cos(phi6)), byrow = T, ncol = 4)
+
+Q6_t <- t(Q6)
+
+A7 <- Q6_t %*% A6 %*% Q6
+
+
+##############
+phi7 <- 1/2 * atan(1/((A7[2,2] -A7[3,3])/(2 * A7[2,3])))
+
+Q7 <- matrix(c(1,0,0,0,
+               0,cos(phi7),-sin(phi7),0,
+               0,sin(phi7),cos(phi7),0,
+               0,0,0,1), byrow = T, ncol = 4)
+
+Q7_t <- t(Q7)
+
+A8 <- Q7_t %*% A7 %*% Q7
+
+eigen(A)
---
+++ WebGL/Lab_6/06.js
@@ -0,0 +1,172 @@
+// 04.js
+
+"use strict";
+
+// Vertex shader program
+const VSHADER_SOURCE =
+    '#version 100\n' +
+  'attribute vec4 a_Position;\n' +
+  'attribute float a_PointSize;\n' +
+  'attribute vec4 a_FragColor;\n' +
+  'varying vec4 v_FragColor;\n' +
+  'void main() {\n' +
+  '  gl_Position = a_Position;\n' +
+  '  gl_PointSize = a_PointSize;\n' +
+  '  v_FragColor = a_FragColor;\n' +
+  '}\n';
+
+// Fragment shader program
+const FSHADER_SOURCE =
+  'precision mediump float;\n' +
+  'varying vec4 v_FragColor;\n' +
+  'uniform float u_Width;\n' +
+  'uniform float u_Height;\n' +
+  'void main() {\n' +
+  'gl_FragColor = vec4(gl_FragCoord.x/u_Width, 0.0, gl_FragCoord.y/u_Height, 1.0);\n' +
+  // 'if (distance(vec2(0.5,0.5), gl_PointCoord) > 0.5)' +
+  // 'discard;\n' +
+  '}\n';
+
+function main() {
+  // Retrieve <canvas> element
+  const canvas = document.getElementById('webgl');
+
+  // Get the rendering context for WebGL
+  const gl = getWebGLContext(canvas);
+  if (!gl) {
+    console.log('Failed to get the rendering context for WebGL');
+    return;
+  }
+
+  // Initialize shaders
+  if (!initShaders(gl, VSHADER_SOURCE, FSHADER_SOURCE)) {
+    console.log('Failed to intialize shaders.');
+    return;
+  }
+
+  const u_Width = gl.getUniformLocation(gl.program, 'u_Width');
+  if (!u_Width) {
+     console.log('Failed to get the storage location of u_Width');
+     return;
+  }
+  
+  gl.uniform1f(u_Width, gl.drawingBufferWidth);
+  
+   const u_Height = gl.getUniformLocation(gl.program, 'u_Height');
+  if (!u_Height) {
+     console.log('Failed to get the storage location of u_Height');
+     return;
+  }
+  gl.uniform1f(u_Height, gl.drawingBufferHeight);
+
+  // Write the positions of vertices to a vertex shader
+  const n = initVertexBuffers(gl);
+  if (n < 0) {
+    console.log('Failed to set the positions of the vertices');
+    return;
+  }
+
+  // Specify the color for clearing <canvas>
+  gl.clearColor(1, 1, 1, 1);
+
+  // Clear <canvas>
+  gl.clear(gl.COLOR_BUFFER_BIT);
+
+  // Draw three points
+    gl.drawArrays(gl.TRIANGLES, 0, n);
+}
+
+function initVertexBuffers(gl) {
+    const n = 3; // The number of vertices
+
+    // const vertices = new Float32Array([0.0, 0.5, -0.5, -0.5, 0.5, -0.5]);
+
+    const vertices = new Float32Array(2 * n);
+
+    vertices[0] = -1.0;
+    vertices[1] = -1.0;
+    vertices[2] = -1.0;
+    vertices[3] = 1.0;
+    vertices[4] = 1.0;
+    vertices[5] = 1.0;
+
+    const vertixSizes = new Float32Array(n);
+
+    vertixSizes[0] = 10.0;
+    vertixSizes[1] = 20.0;
+    vertixSizes[2] = 30.0;
+
+
+	const vertixColors = new Float32Array(4*n);
+
+    vertixColors[0] = 1.0;
+    vertixColors[1] = 0.0;
+    vertixColors[2] = 0.0;
+	vertixColors[3] = 1.0;
+	
+	vertixColors[4] = 0.0;
+    vertixColors[5] = 1.0;
+    vertixColors[6] = 0.0;
+	vertixColors[7] = 1.0;
+	
+	vertixColors[8] = 0.0;
+    vertixColors[9] = 0.0;
+    vertixColors[10] = 1.0;
+	vertixColors[11] = 1.0;
+  // Create a buffer object
+  const vertexBuffer = gl.createBuffer();
+  if (!vertexBuffer) {
+    console.log('Failed to create the buffer object');
+    return -1;
+  }
+
+  // Bind the buffer object to target
+  gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
+  // Write date into the buffer object
+  var FSIZE = vertices.BYTES_PER_ELEMENT;
+  gl.bufferData(gl.ARRAY_BUFFER, 7*n*FSIZE, gl.STATIC_DRAW);
+  
+   const a_PointSize = gl.getAttribLocation(gl.program, 'a_PointSize');
+  if (a_PointSize < 0) {
+    console.log('Failed to get the storage location of a_Position');
+    return -1;
+  }
+   gl.bufferSubData(gl.ARRAY_BUFFER,0,vertixSizes);
+  //Assign the buffer object to a_PointSize variable
+  gl.vertexAttribPointer(a_PointSize, 1, gl.FLOAT, false, 0,0 );
+
+  //Enable the assignment to a_PointSize variable
+  gl.enableVertexAttribArray(a_PointSize);
+  
+  const a_Position = gl.getAttribLocation(gl.program, 'a_Position');
+  if (a_Position < 0) {
+    console.log('Failed to get the storage location of a_Position');
+    return -1;
+  }
+  gl.bufferSubData(gl.ARRAY_BUFFER,n*FSIZE,vertices)
+  // Assign the buffer object to a_Position variable
+  gl.vertexAttribPointer(a_Position, 2, gl.FLOAT, false, 0, n*FSIZE);
+  
+  // Enable the assignment to a_Position variable
+  gl.enableVertexAttribArray(a_Position);
+
+    const a_FragColor = gl.getAttribLocation(gl.program, 'a_FragColor');
+  if (a_FragColor < 0) {
+    console.log('Failed to get the storage location of a_FragColor');
+    return -1;
+  }
+  
+   gl.bufferSubData(gl.ARRAY_BUFFER,3*n*FSIZE,vertixColors);
+  //Assign the buffer object to a_FragColor variable
+  gl.vertexAttribPointer(a_FragColor, 4, gl.FLOAT, false, 0,3*n*FSIZE );
+
+  //Enable the assignment to a_FragColor variable
+  gl.enableVertexAttribArray(a_FragColor);
+  
+ 
+  
+
+  return n;
+}
+
+
